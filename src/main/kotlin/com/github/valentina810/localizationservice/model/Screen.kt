71261ec144530c4f768ff0a9f56d2a48316dbcf7
valentinakolesnikova <valentinavasileva34@gmail.com>
2025-05-04 21:15:30 +0300

    feat: Добавить  ручку для получения нужного экрана GET, поменять структуру данных в БД

---
+++ src/main/kotlin/com/github/valentina810/localizationservice/model/Screen.kt
@@ -0,0 +1,6 @@
+package com.github.valentina810.localizationservice.model
+
+data class Screen(
+    val screen: String,
+    val elements: Map<String, Any>
+)
--- src/main/kotlin/com/github/valentina810/localizationservice/repository/ScreenTranslationRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.github.valentina810.localizationservice.repository
-
-import com.github.valentina810.localizationservice.model.ScreenTranslation
-import org.springframework.data.mongodb.repository.MongoRepository
-import java.util.Optional
-
-interface ScreenTranslationRepository : MongoRepository<ScreenTranslation, String> {
-    fun findByScreenNameAndLocale(screenName: String, locale: String): Optional<ScreenTranslation>
-}
